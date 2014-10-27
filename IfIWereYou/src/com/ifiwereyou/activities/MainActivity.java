e40af76aa2e2e5c6e09c31f5e5ca22052727ac96
Simon Tenbeitel <simon.tenbeitel@sap.com>
2014-10-27 09:28:37 +0100

    Updated Activites and Contact Screenshot added
    Change-Id: I768635def851d072850c42dbcb67239a9b41e6e2

--- IfIWereYou/src/com/ifiwereyou/activities/MainActivity.java
+++ IfIWereYou/src/com/ifiwereyou/activities/MainActivity.java
@@ -1,14 +1,17 @@
 package com.ifiwereyou.activities;
 
+/*
+ * Copyright 2014 by Axel Müller, Hendrik Böwer, Simon Tenbeitel
+ */
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 	}
-	
 }

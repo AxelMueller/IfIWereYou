449c6ec33ccc9457b91d134af83d173f1732aed1
Simon Tenbeitel <simon.tenbeitel@sap.com>
2015-03-31 18:15:31 +0200

    IWEREYOU-28 Implemented Facebook Login

--- IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/activities/LoginScreenActivity.java
+++ IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/activities/LoginScreenActivity.java
@@ -1,107 +1,68 @@
 package com.ifiwereyou.activities;
 
-import java.util.Arrays;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.util.Log;
 import android.view.Window;
-import android.widget.Button;
 
-import com.facebook.AppEventsLogger;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.widget.LoginButton;
 import com.ifiwereyou.R;
-import com.parse.ParseObject;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginScreenActivity extends Activity {
 
-	Button loginButton;
-	LoginButton facebookLogin;
 	private static final String TAG = "LoginScreenActivity";
-	private UiLifecycleHelper uiHelper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		uiHelper = new UiLifecycleHelper(this, callback);
-		uiHelper.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_login_screen);
-		loginButton = (Button) findViewById(R.id.loginButton);
-		loginButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO: Call Login with E-Mail Screen and not MainActivity
-				Intent intent = new Intent(LoginScreenActivity.this,
-						EmailLoginActivity.class);
-				startActivity(intent);
-				finish();
-			}
-		});
-		facebookLogin = (LoginButton) findViewById(R.id.authButton);
-		facebookLogin.setReadPermissions(Arrays.asList("user_likes",
-				"user_status"));
+        ButterKnife.inject(this);
 	}
 
-	private void onSessionStateChange(Session session, SessionState state,
-			Exception exception) {
-		if (state.isOpened()) {
-			Intent intent = new Intent(LoginScreenActivity.this,
-					MainActivity.class);
-			startActivity(intent);
-			finish();
-		}
-	}
+    @OnClick(R.id.loginButton)
+    public void emailLogin() {
+        Intent intent = new Intent(LoginScreenActivity.this,
+                EmailLoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
-	private Session.StatusCallback callback = new Session.StatusCallback() {
-		@Override
-		public void call(Session session, SessionState state,
-				Exception exception) {
-			onSessionStateChange(session, state, exception);
-		}
-	};
+    @OnClick(R.id.facebookLogin)
+    public void facebookLogin() {
+        ParseFacebookUtils.logIn(this, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException err) {
+                if (user == null) {
+                    Log.d("If I Were You", "Uh oh. The user cancelled the Facebook login.");
+                    return;
+                } else if (user.isNew()) {
+                    Log.d("If I Were You", "User signed up and logged in through Facebook!");
+                } else {
+                    Log.d("If I Were You", "User logged in through Facebook!");
+                }
+                goToMainActivity();
+            }
+        });
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		Session session = Session.getActiveSession();
-		if (session != null && (session.isOpened() || session.isClosed())) {
-			onSessionStateChange(session, session.getState(), null);
-		}
-		uiHelper.onResume();
-		// Logs 'install' and 'app activate' App Events.
-		AppEventsLogger.activateApp(this);
-	}
+    private void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		uiHelper.onPause();
-		// Logs 'app deactivate' App Event.
-		AppEventsLogger.deactivateApp(this);
-	}
-
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		uiHelper.onActivityResult(requestCode, resultCode, data);
-	}
-
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		uiHelper.onDestroy();
-	}
-
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		uiHelper.onSaveInstanceState(outState);
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+    }
 
 }

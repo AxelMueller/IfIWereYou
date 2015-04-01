eb116d3c7714e022738672cfa32a54b0335bb939
Simon Tenbeitel <simon.tenbeitel@sap.com>
2015-04-01 14:21:31 +0200

    Keep user signed in

--- IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/IfIWereYouApplication.java
+++ IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/IfIWereYouApplication.java
@@ -1,6 +1,7 @@
 package com.ifiwereyou;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
@@ -10,6 +11,8 @@ import com.parse.ParseFacebookUtils;
  */
 public class IfIWereYouApplication extends Application {
 
+    private static Context sContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -18,6 +21,12 @@ public class IfIWereYouApplication extends Application {
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "uahSAQ1KHf8VbW1gJ1hHcLTbJ3zgZqWO0mpGZRPR", "uiK17aiooWqfnk9J13SBjCz4MduKGsjjsT4tM8mW");
         ParseFacebookUtils.initialize();
+
+        sContext = this;
+    }
+
+    public static Context getContext() {
+        return sContext;
     }
 
 }

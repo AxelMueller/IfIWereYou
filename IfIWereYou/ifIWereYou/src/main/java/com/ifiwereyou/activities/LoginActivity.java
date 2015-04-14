404d43b819189377d6a20dc470565e78f9f6a8f8
Simon Tenbeitel <simon.tenbeitel@sap.com>
2015-04-14 11:43:36 +0200

    IWEREYOU-28 Implemented Facebook Login

---
+++ IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/activities/LoginActivity.java
@@ -0,0 +1,132 @@
+package com.ifiwereyou.activities;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.ifiwereyou.R;
+import com.ifiwereyou.provider.LoginFragmentPagerAdapter;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by D060336 on 13.04.2015.
+ */
+public class LoginActivity extends FragmentActivity{
+
+    FragmentPagerAdapter mPagerAdapter;
+    @InjectView(R.id.loginViewPager) ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ParseUser.getCurrentUser() != null)
+            goToMainActivity();
+
+        setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
+        setupTabs();
+    }
+
+    private void setupTabs() {
+        final ActionBar actionBar = getActionBar();
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mPagerAdapter = new LoginFragmentPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+        mViewPager
+                .setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        // When swiping between pages, select the corresponding
+                        // tab. This is necessary because the pages are not
+                        // linked to the tabs by default.
+                        getActionBar().setSelectedNavigationItem(position);
+                    }
+                });
+
+        // Specify that tabs should be displayed in the action bar.
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        actionBar.setIcon(new ColorDrawable(getResources().getColor(
+                android.R.color.transparent))); // FIXME: Later this should not be in the source code.
+
+        // Create a tab listener that is called when the user changes tabs.
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // TODO Auto-generated method stub
+                // show the given tab
+                // When the tab is selected, switch to the
+                // corresponding page in the ViewPager.
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // TODO Auto-generated method stub
+                // hide the given tab
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+                // TODO Auto-generated method stub
+                // probably ignore this event
+            }
+        };
+
+        // Add 4 tabs to the action bar and specify the tab's text and
+        // TabListener
+        // TODO: Decide whether to use fixed tabs or scrollable tabs:
+        // http://developer.android.com/design/building-blocks/tabs.html
+        for (int i = 0; i < mPagerAdapter.getCount(); i++) {
+            actionBar.addTab(actionBar.newTab()
+                    .setText(mPagerAdapter.getPageTitle(i))
+                    .setTabListener(tabListener));
+        }
+
+        try {
+            Method setHasEmbeddedTabsMethod = actionBar.getClass()
+                    .getDeclaredMethod("setHasEmbeddedTabs", boolean.class);
+            setHasEmbeddedTabsMethod.setAccessible(true);
+            setHasEmbeddedTabsMethod.invoke(actionBar, false);
+        } catch (NoSuchMethodException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+    }
+
+}
--- IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/fragments/AddContactFragment.java
+++ IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/fragments/AddContactFragment.java
@@ -13,70 +13,93 @@ import android.widget.Toast;
 import com.ifiwereyou.R;
 import com.ifiwereyou.provider.ServerFunctions;
 import com.ifiwereyou.utils.UserInputCheck;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class AddContactFragment extends Fragment {
 
-	EditText emailEditText;
-	Button addButton;
+    EditText emailEditText;
+    Button addButton;
 
-	public AddContactFragment() {
+    public AddContactFragment() {
 
-	}
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_add_contact,
-				container, false);
-		emailEditText = (EditText) rootView
-				.findViewById(R.id.add_contact_emailEditText);
-		addButton = (Button) rootView.findViewById(R.id.add_contact_addButton);
-		addButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				String email = emailEditText.getText().toString();
-				if (!UserInputCheck.isValidEmail(email)) {
-					Toast.makeText(
-							getActivity().getApplicationContext(),
-							getActivity()
-									.getResources()
-									.getString(
-											R.string.add_contact_email_not_valid_message),
-							Toast.LENGTH_LONG).show();
-					return;
-				}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_add_contact,
+                container, false);
+        emailEditText = (EditText) rootView
+                .findViewById(R.id.add_contact_emailEditText);
+        addButton = (Button) rootView.findViewById(R.id.add_contact_addButton);
+        addButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = emailEditText.getText().toString();
+                if (!UserInputCheck.isValidEmail(email)) {
+                    Toast.makeText(
+                            getActivity().getApplicationContext(),
+                            getActivity()
+                                    .getResources()
+                                    .getString(
+                                            R.string.add_contact_email_not_valid_message),
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-				ServerFunctions server = new ServerFunctions();
-				try {
-					if (server.addFriend(getActivity(), email)) {
-						Toast.makeText(
-								getActivity(),
-								getActivity().getResources().getString(
-										R.string.add_contact_success),
-								Toast.LENGTH_LONG).show();
-					}
-				} catch (Exception e) {
-					if (e.getMessage() != null
-							&& e.getMessage().equals(
-									"No user with the given mail address")) {
-						Toast.makeText(
-								getActivity().getApplicationContext(),
-								String.format(
-										getActivity()
-												.getResources()
-												.getString(
-														R.string.add_contact_fail_message),
-										email), Toast.LENGTH_LONG).show();
-					} else {
-						Toast.makeText(
-								getActivity().getApplicationContext(),
-								getActivity().getResources().getString(
-										R.string.add_contact_already_exists),
-								Toast.LENGTH_LONG).show();
-					}
-				}
-			}
-		});
-		return rootView;
-	}
+                ParseQuery<ParseUser> query = ParseUser.getQuery();
+                query.whereEqualTo("email", email);
+                query.findInBackground(new FindCallback<ParseUser>() {
+                    public void done(List<ParseUser> objects, ParseException e) {
+                        if (e != null || objects == null || objects.isEmpty()) {
+                            Toast.makeText(
+                                    getActivity().getApplicationContext(),
+                                    String.format(
+                                            getString(R.string.add_contact_fail_message),
+                                            email), Toast.LENGTH_LONG).show();
+                        } else {
+                            // the query was successful
+
+                        }
+                    }
+                });
+
+                ServerFunctions server = new ServerFunctions();
+                try {
+                    if (server.addFriend(getActivity(), email)) {
+                        Toast.makeText(
+                                getActivity(),
+                                getActivity().getResources().getString(
+                                        R.string.add_contact_success),
+                                Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    if (e.getMessage() != null
+                            && e.getMessage().equals(
+                            "No user with the given mail address")) {
+                        Toast.makeText(
+                                getActivity().getApplicationContext(),
+                                String.format(
+                                        getActivity()
+                                                .getResources()
+                                                .getString(
+                                                        R.string.add_contact_fail_message),
+                                        email), Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(
+                                getActivity().getApplicationContext(),
+                                getActivity().getResources().getString(
+                                        R.string.add_contact_already_exists),
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+        return rootView;
+    }
 }

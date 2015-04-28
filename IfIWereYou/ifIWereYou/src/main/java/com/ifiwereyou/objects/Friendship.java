b9fa84d01423c71b733595cdfaeb59b6d9c20f2f
Simon Tenbeitel <simon.tenbeitel@sap.com>
2015-04-28 15:13:04 +0200

    Extended Friendship with getFriend method

--- IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/objects/Friendship.java
+++ IfIWereYou/ifIWereYou/src/main/java/com/ifiwereyou/objects/Friendship.java
@@ -3,6 +3,7 @@ package com.ifiwereyou.objects;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 /**
  * Created by D060426 on 23.04.2015.
@@ -19,6 +20,13 @@ public class Friendship extends ParseObject{
 
     public String getFriendB() {
         return getString(KEY_FRIENDB);
+    }
+
+    public ParseUser getFriend() {
+        String currentUserId = ParseUser.getCurrentUser().getObjectId();
+        return (getParseObject(KEY_FRIENDA).getObjectId().equals(currentUserId) && !getParseObject(KEY_FRIENDB).getObjectId().equals(currentUserId)) ? (ParseUser) getParseObject(KEY_FRIENDA)
+                : (getParseObject(KEY_FRIENDB).getObjectId().equals(currentUserId) && !getParseObject(KEY_FRIENDA).getObjectId().equals(currentUserId)) ? (ParseUser) getParseObject(KEY_FRIENDB)
+                    : null;
     }
 
     public static ParseQuery<Friendship> getQuery(){
